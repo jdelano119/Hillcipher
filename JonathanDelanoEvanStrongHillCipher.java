92eadc4da192bd3a0fe7381f71d1b11b3da6a926
Jack Delano <delanojx@dukes.jmu.edu>
2025-10-18 21:38:50 -0400

    hi i luv u

---
+++ JonathanDelanoEvanStrongHillCipher.java
@@ -0,0 +1,16 @@
+public class JonathanDelanoEvanStrongHillCipher {
+
+    
+    int[][] findDecryptionKey(int encryptionKey[2][2]){
+
+    }
+
+    int[] encrypt(int plaintext[], int encryptionKey[2][2]){
+
+    }
+    int[] decrypt(int ciphertext[], int decryptionKey[2][2]){
+
+    }
+
+
+}
