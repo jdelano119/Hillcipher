081fb1627f87822b11e511d956ce633ff73451ea
Jack Delano <delanojx@dukes.jmu.edu>
2025-10-18 21:48:59 -0400

    hello

--- JonathanDelanoEvanStrongHillCipher.java
+++ JonathanDelanoEvanStrongHillCipher.java
@@ -1,75 +1,39 @@
-public int gcd (int inE, int inZ) {
-		// TO BE FINISHED
-		// Must implement Euclid's algorithm
-		// NO brute-forcing; violation will lead to zero points
-		// NO recursion; violation will lead to zero points
-		int d = inZ;
-		int q;
-		int g = inE;
-
-		while (g != 0){
-			q = d % g;
-			d = g;
-			g = q;
-		}
-		return d;
-		
-	}
-
-	public void testGcd () {
-		int result1 = gcd (29, 288);
-		int result2 = gcd (30, 288);
-		int result3 = gcd (149, 288);
-		int result4 = gcd (2, 4);
-
-		System.out.println ("GCD (29, 288) = 0x" + Integer.toString(result1, 16));
-		System.out.println ("GCD (30, 288) = 0x" + Integer.toString(result2, 16));
-		System.out.println ("GCD (149, 288) = 0x" + Integer.toString(result3, 16));
-		System.out.println ("GCD (2, 4) = 0x" + Integer.toString(result4, 16));
-	}
-	
-	//
-	// We assume that inE < inZ
-	// This implementation follows our slides
-	// Return:
-	//	-1: no inverse
-	//	inverse of inE mod inZ
-	//
-	public int xgcd(int inE, int inZ) {
-    int T = 0;
-    int newT = 1;
-    int D = inZ;
-    int newD = inE;
-
-    while (newD != 0) {
-        int q = D / newD;
-
-        int tempD = D - q * newD;
-        D = newD;
-        newD = tempD;
-
-        int tempT = T - q * newT;
-        T = newT;
-        newT = tempT;
-    }
-    if (D != 1) {
-        return -1;
-    }
-    return (T % inZ + inZ) % inZ;
-}
 public class JonathanDelanoEvanStrongHillCipher {
+    public int xgcd(int inE, int inZ) {
+        int T = 0;
+        int newT = 1;
+        int D = inZ;
+        int newD = inE;
 
-    
-    int[][] findDecryptionKey(int encryptionKey[2][2]){
+        while (newD != 0) {
+            int q = D / newD;
 
+            int tempD = D - q * newD;
+            D = newD;
+            newD = tempD;
+
+            int tempT = T - q * newT;
+            T = newT;
+            newT = tempT;
+        }
+        if (D != 1) {
+            return -1;
+        }
+        return (T % inZ + inZ) % inZ;
     }
 
-    int[] encrypt(int plaintext[], int encryptionKey[2][2]){
+    int[][] findDecryptionKey(int[][] encryptionKey){
 
-    }
-    int[] decrypt(int ciphertext[], int decryptionKey[2][2]){
-
+        return new int[2][2];
     }
 
+    int[] encrypt(int[] plaintext, int[][] encryptionKey){
+        int[] ret = new int[1];
+        return ret;
+    }
+    int[] decrypt(int[] ciphertext, int[][] decryptionKey){
+        int[] ret = new int[1];
+        return ret;
+    }
 
 }
